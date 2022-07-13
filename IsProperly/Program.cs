667aa323677ec57906ed570f90c5becb85a926a8
zuka <zuka@DESKTOP-01OQQMQ>
2022-07-13 21:36:54 +0400

    Add project files.

---
+++ IsProperly/Program.cs
@@ -0,0 +1,18 @@
+﻿static bool IsProperly(string sequence)
+{
+    int n = 0;
+    foreach (char c in sequence)
+    {
+        if (n < 0)
+        {
+            return false;
+        }
+        int x = (c == '(' ? 1 : -1);
+        n += x;
+    }
+    if (n == 0)
+    {
+        return true;
+    }
+    return false;
+}
